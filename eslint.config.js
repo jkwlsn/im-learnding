2166f8e8f7c9a569bfbaf8eb5675d0eacaed6b65
Jacob Wilson <hello@jacobcharleswilson.com>
2025-03-26 13:41:36 +0000

    Updates eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
-// eslint.config.js
-import { defineConfig } from "eslint/config";
+const html = require("@html-eslint/eslint-plugin");
 
-export default defineConfig([
-    {
-        rules: {
-            semi: "error",
-            "prefer-const": "error"
-        }
-    }
-]);
+module.exports = [
+  // your own configurations.
+  {
+    // recommended configuration included in the plugin
+    ...html.configs["flat/recommended"],
+    files: ["**/*.html"],
+    rules: {
+      ...html.configs["flat/recommended"].rules, // Must be defined. If not, all recommended rules will be lost
+      "@html-eslint/no-multiple-h1": 0,
+    },
+  },
+];
