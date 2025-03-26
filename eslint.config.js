aaf8219bca9161a85b99c259ff0ce6a2a79e88f4
Jacob Wilson <hello@jacobcharleswilson.com>
2025-03-26 14:31:12 +0000

    --amend

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
 const html = require("@html-eslint/eslint-plugin");
 
 module.exports = [
-  // your own configurations.
   {
-    // recommended configuration included in the plugin
     ...html.configs["flat/recommended"],
     files: ["**/*.html"],
     rules: {
